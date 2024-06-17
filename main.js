daa1abc8625799a7db3a9f4ce98c0b8e4ba70c3b
tazl <82299965+tazlsucks@users.noreply.github.com>
2024-06-17 19:57:44 -0400

    cleanup

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+
+function toggleWork() {
+    // Toggle the display of both pages
+    var page1 = document.querySelector('.page1');
+    var works = document.querySelector('.works');
+
+    if (page1.style.display === 'none') {
+        page1.style.display = 'block';
+        works.style.display = 'none';
+    } else {
+        page1.style.display = 'none';
+        works.style.display = 'block';
+    }
+}
+function toggleAbout() {
+        // Toggle the display of both pages
+        var page1 = document.querySelector('.page1');
+        var about = document.querySelector('.about');
+
+        if (page1.style.display === 'none') {
+            page1.style.display = 'block';
+            about.style.display = 'none';
+        } else {
+            page1.style.display = 'none';
+            about.style.display = 'block';
+        }
+    }
