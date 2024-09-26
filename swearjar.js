714fffa780b92ddd64535d8ade04a89264bbae41
tazl <82299965+tazlsucks@users.noreply.github.com>
2024-09-25 20:54:35 -0400

    are we ready people?!!

--- swearjar.js
+++ swearjar.js
@@ -1,13 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    toggleSectionBasedOnHash();
+    window.onhashchange = toggleSectionBasedOnHash;
+});
+
+
 function toggleSectionBasedOnHash() {
     const hash = window.location.hash;
     const page1 = document.querySelector('.page1');
     const privacy = document.querySelector('.privacy');
 
-    // Hide all sections initially
     privacy.style.display = 'none';
     page1.style.display = 'none';
 
-    // Show the section based on the hash
     if (hash === '#privacy') {
         privacy.style.display = 'block';
     } else {
